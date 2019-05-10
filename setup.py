23c81946da793d743f54b742ce64240224b5906c
LePetitTim <timothee.de_montety@isen.yncrea.fr>
2019-05-10 16:29:56 +0200

    Change setup

--- setup.py
+++ setup.py
@@ -16,12 +16,7 @@ setup(
         url='https://github.com/makinacorpus/django-datastore',
         download_url="",
         description="",
-        long_description=codecs.open(
-            os.path.join(
-                file_setup, 'README.rst'), 'r', 'utf-8').read() + '\n\n' +
-                         codecs.open(
-                             os.path.join(file_setup, 'CHANGES'),
-                             'r', 'utf-8').read(),
+        long_description="",
         license='MIT, see LICENSE file.',
         install_requires=requires,
         packages=find_packages(exclude=("tests",)),
@@ -33,6 +28,7 @@ setup(
                      'Intended Audience :: Developers',
                      'Environment :: Web Environment',
                      'Framework :: Django',
+                     'License :: OSI Approved :: MIT License',
                      'Programming Language :: Python :: 3.6',
                      'Programming Language :: Python :: 3.7'],
 )
